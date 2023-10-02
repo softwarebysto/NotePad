1d3f29483c15d591a752ee514a10749859fa2cbd
softwarebysto <93490640+softwarebysto@users.noreply.github.com>
2023-10-02 14:15:22 +0500

    Add project files.

---
+++ Models/APIModes/GetNotesModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.APIModes
+{
+    public class GetNotesModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Conent { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime? ModifiedTime { get; set; }
+
+    }
+}
